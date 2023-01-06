34a3079baea0eed72dc9abebf5ecdaf46e0b54c0
anactualfox <foxablessc2@gmail.com>
2023-01-06 05:46:16 -0700

    statistics can now display win ratios

--- Analyzer.cs
+++ Analyzer.cs
@@ -1,41 +1,84 @@
-﻿using System.Collections.Generic;
+﻿using Avalonia.Controls;
+using Avalonia.Layout;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
-// Analyzes matches to calculate match up win rates and get apm/eapm graph
-namespace srra
+
+namespace srra;
+
+public class Analyzer
 {
-    public class Analyzer
+    MainWindow _mainWindow;
+    MainWindowViewModel _mainWindowViewModel;
+    public WinRates? WinRates;
+
+    public Analyzer(MainWindow mainWindow, MainWindowViewModel mainWindowViewModel)
     {
-        MainWindow _mainWindow;
-        MainWindowViewModel _mainWindowViewModel;
+        _mainWindow = mainWindow;
+        _mainWindowViewModel = mainWindowViewModel;
+    }
 
-        public Analyzer(MainWindow mainWindow, MainWindowViewModel mainWindowViewModel)
-        {
-            _mainWindow = mainWindow;
-            _mainWindowViewModel = mainWindowViewModel;
-        }
+    public void UpdateGraphData()
+    {
+        var apmResults = GetAPMResults();
+        var xData = Enumerable.Range(0, apmResults.Count).Select(x => (double)x).ToArray();
+        var graph = new Graph(_mainWindow.StatisticsPlot, "APM Graph") {
+            xData = xData,
+            yData = apmResults.Select(x => (double?)x ?? 0.0).ToArray()
+        };
+        graph.ShowGraph();
+    }
 
-        public void ShowGraphData()
-        {
-            var apmResults = GetAPMResults();
-            var xData = Enumerable.Range(0, apmResults.Count).Select(x => (double)x).ToArray();
-            var graph = new Graph(_mainWindow.StatisticsPlot, "APM Graph") {
-                xData = xData,
-                yData = apmResults.Select(x => (double?)x ?? 0.0).ToArray()
-            };
-            graph.ShowGraph();
-        }
+    public void AnalyzeReplays(List<Match> matches)
+    {
+        FindWinRatios(matches);
+        UpdateWinRatios();
+    }
 
-        private List<int?> GetAPMResults()
-        {
-            var apmResults = new List<int?>();
-            var playerName = ConfigurationManager.AppSettings["PlayerName"];
-            foreach (var match in _mainWindowViewModel.Matches) {
-                var player = match?.Players?.Find(player => player.Name == playerName);
-                if (player is null) continue;
-                apmResults.Add(player.APM);
+    public void FindWinRatios(List<Match> matches)
+    {
+        var playerName = ConfigurationManager.AppSettings["PlayerName"];
+        if (playerName is null)
+            return;
+
+        WinRates = new WinRates();
+        matches.ForEach(match => {
+            var player = match.Players.Find(p => p.Name == playerName);
+            if (player != null) {
+                var opponent = match.Players.Find(p => p.ID != player.ID)!;
+                WinRates[player.Race!][opponent.Race!]["Games"]++;
+                if (match.WinnerTeam == player.TeamID)
+                    WinRates[player.Race!][opponent.Race!]["Wins"]++;
             }
-            return apmResults;
+        });
+    }
+
+    public void UpdateWinRatios()
+    {
+        var races = new List<string>() { "Terran", "Zerg", "Protoss" };
+
+        foreach (var player in races.Select((race, index) => (race, index))) 
+            foreach (var opponent in races.Select((race, index) => (race, index))) {
+                var textBlock = new TextBlock() {
+                    Text = $"{Match.GetRaceAlias(player.race)}v{Match.GetRaceAlias(opponent.race)}: {WinRates[player.race][opponent.race].GetWinRate():P}",
+                    [Grid.RowProperty] = player.index + 1,
+                    [Grid.ColumnProperty] = opponent.index + 1,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center,
+                };
+                _mainWindow.WinRatesGrid.Children.Add(textBlock);
+            }
+    }
+
+    private List<int?> GetAPMResults()
+    {
+        var apmResults = new List<int?>();
+        var playerName = ConfigurationManager.AppSettings["PlayerName"];
+        foreach (var match in _mainWindowViewModel.Matches) {
+            var player = match?.Players?.Find(player => player.Name == playerName);
+            if (player is null) continue;
+            apmResults.Add(player.APM);
         }
+        return apmResults;
     }
 }
