0a6e45af4dcd1620535029a2eb063d8c524392ed
anactualfox <foxablessc2@gmail.com>
2023-01-06 05:47:02 -0700

    removed unecessary extensions

--- Extensions.cs
+++ Extensions.cs
@@ -8,22 +8,6 @@ namespace srra;
 
 public static class Extensions
 {
-    public static async Task<int> StartAsync(this Process process)
-    {
-        process.Start();
-        process.BeginOutputReadLine();
-        process.BeginErrorReadLine();
-        await process.WaitForExitAsync();
-        return process.ExitCode;
-    }
-
-    public static string RemoveWhitespace(this string input)
-    {
-        return new string(input.ToCharArray()
-            .Where(c => !char.IsWhiteSpace(c))
-            .ToArray());
-    }
-
     public static Dictionary<string, JsonElement>? GetNestedJsonObject(this JsonElement jsonElement)
     {
         return JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(JsonSerializer.Serialize(jsonElement));
