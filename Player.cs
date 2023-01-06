0c95ca09dde4273271ca8c6cfa456186cb6a6c08
anactualfox <foxablessc2@gmail.com>
2023-01-05 17:29:45 -0700

    added class to represent player

---
+++ Player.cs
@@ -0,0 +1,21 @@
+﻿namespace srra
+{
+    public class Player
+    {
+        public int? ID;
+        public int? TeamID;
+        public string? Name;
+        public int? APM;
+        public int? EAPM;
+        public string? Race;
+        public Player(int? id, int? teamId, string? name, int? apm, int? eapm, string? race)
+        {
+            ID = id;
+            TeamID = teamId;
+            Name = name;    
+            APM = apm;
+            EAPM = eapm;
+            Race = race; 
+        }
+    }
+}
