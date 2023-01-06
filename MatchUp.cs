7f698e3adda07b205cdf2ed87ad05e9616b92dcb
anactualfox <foxablessc2@gmail.com>
2023-01-06 06:05:03 -0700

    added class to represent a matchup

---
+++ MatchUp.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace srra
+{
+    public class WinRates
+    {
+        public readonly Dictionary<string, Matchup> _winRates = new() {
+            {"Terran", new Matchup() },
+            {"Protoss", new Matchup() },
+            {"Zerg", new Matchup() },
+        };
+        public Matchup this[string race] { get => _winRates[race]; set => _winRates[race] = value; }
+    }
+
+    public class MatchResults
+    {
+        private readonly Dictionary<string, int> results = new() {
+            { "Wins", 0 },
+            { "Games", 0 }
+        };
+        public MatchResults() { }
+        public int this[string key] { get => results[key]; set => results[key] = value; }
+        public ICollection Keys => results.Keys;
+        public ICollection Values => results.Values;
+        public bool Contains(string key) => results.ContainsKey(key);
+        public double GetWinRate() => (double)this["Wins"] / this["Games"];
+    }
+
+    public class Matchup
+    {
+        readonly Dictionary<string, MatchResults> matchup = new() {
+            {"Terran", new MatchResults() },
+            {"Protoss", new MatchResults() },
+            {"Zerg", new MatchResults() },
+
+        };
+        public MatchResults this[string opponentRace] { get => matchup[opponentRace]; set => matchup[opponentRace] = value; }
+    }
+}
