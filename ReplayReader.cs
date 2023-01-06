5bd59e81dc343e5e2f0032a72f09bd2667a2bba3
anactualfox <foxablessc2@gmail.com>
2023-01-05 22:14:23 -0700

    split up code into more functions

--- ReplayReader.cs
+++ ReplayReader.cs
@@ -3,6 +3,7 @@ using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Configuration;
 
 namespace srra;
 
@@ -10,42 +11,32 @@ public class ReplayReader
 {
     readonly MainWindow _mainWindow;
     readonly MainWindowViewModel _mainWindowVM;
-    public List<string> replayData = new();
+    public List<Match> replayData = new();
     public List<Thread> processingThreads = new();
-    private readonly string? _screpPath;
     private readonly List<string>? _replayPaths;
-    private string? _incomingData;
+    private byte count = 0;
 
-
-    public ReplayReader(MainWindow mainWindow, MainWindowViewModel mainWindowVM, string screpPath, List<string> replayPaths)
+    public ReplayReader(MainWindow mainWindow, MainWindowViewModel mainWindowVM, List<string> replayPaths)
     {
         _mainWindow = mainWindow;
         _mainWindowVM = mainWindowVM;   
-        _screpPath = screpPath;
         _replayPaths = replayPaths;
     }
+
     public async Task ReadReplay(string replayPath)
     {
+        string? _screpPath = ConfigurationManager.AppSettings["SCREP_Path"];
         if (_screpPath is null || _replayPaths?.Count == 0) return;
         try {
-            using var proc = new Process() {
-                StartInfo = new ProcessStartInfo() {
-                    UseShellExecute = false,
-                    FileName = $"{_screpPath}\\screp.exe",
-                    CreateNoWindow = true,
-                    Arguments = $"-map \"{replayPath}\"",
-                    RedirectStandardOutput = true,
-                },
-            };
-            proc.Start();
-            _incomingData = await proc.StandardOutput.ReadToEndAsync();
-            // Success exit code from parser
-            if (proc.ExitCode != 0) return;
-            var match = new Match(_incomingData);
-            if (match.IsLadderMatch)
+            var data = await ReadFromSCREP(_screpPath, replayPath);
+            if (data is null) return;
+            var match = new Match(data, replayPath);
+            if (match.IsLadderMatch) {
                 _mainWindowVM.Matches.Add(match);
+                replayData.Add(match);
+            }
         }
-        catch (Exception e) {
+        catch (Exception) {
             // Ignored
         }
     }
@@ -53,14 +44,36 @@ public class ReplayReader
     public async Task ReadReplays()
     {
         if (_replayPaths == null) return;
-        var count = 0;
-        (new Action(() => _mainWindow.SRRAProgressBar.IsVisible = true)).Invoke();
         foreach (var replayPath in _replayPaths) {
             await ReadReplay(replayPath);
-            (new Action(() => _mainWindow.SRRAProgressBar.Value = (++count * 100) / _replayPaths.Count)).Invoke();
+            UpdateProgressBar();
         }
-        if (count == _replayPaths.Count) {
+    }
+
+    private static async Task<string?> ReadFromSCREP(string screpPath, string replayPath)
+    {
+        using var proc = new Process() {
+            StartInfo = new ProcessStartInfo() {
+                UseShellExecute = false,
+                FileName = $"{screpPath}\\screp.exe",
+                CreateNoWindow = true,
+                Arguments = $"-map \"{replayPath}\"",
+                RedirectStandardOutput = true,
+            },
+        };
+        proc.Start();
+        var data = await proc.StandardOutput.ReadToEndAsync();
+        return proc.ExitCode == 0 ? data : null;
+    }
+
+    private void UpdateProgressBar()
+    {
+        if (_replayPaths == null) return;
+        if (count == 0)
+            (new Action(() => _mainWindow.SRRAProgressBar.IsVisible = true)).Invoke();
+        if (count == _replayPaths.Count - 1)
             (new Action(() => _mainWindow.SRRAProgressBar.IsVisible = false)).Invoke();
-        }
+
+        new Action(() => _mainWindow.SRRAProgressBar.Value = (++count * 100) / _replayPaths.Count).Invoke();
     }
 }
