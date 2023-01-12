8db8fe49aa2c4c1e4067131e554fd4aa539e9786
anactualfox <foxablessc2@gmail.com>
2023-01-12 08:12:30 -0700

    .

--- Starcraft/Match.cs
+++ Starcraft/Match.cs
@@ -48,7 +48,7 @@ namespace srra.Starcraft
 
         public override string ToString() => $"{FilePath}";
 
-        public override bool Equals(object obj)
+        public override bool Equals(object? obj)
         {
             if (obj is null) return false;
             if (obj is not Match objectAsMatch) return false;
